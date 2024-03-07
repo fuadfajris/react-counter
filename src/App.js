59ff442a2d66234943bebcc217257bbe91a96615
Fuad.Soba <fuad.soba@astralife.co.id>
2024-03-07 14:43:12 +0700

    update component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { useState } from "react";
-import plusIcon from "./assets/plus-icon.svg"
-import minusIcon from "./assets/minus-icon.svg";
 import Navbar from "./components/Navbar";
 import Container from "./components/Container";
+import SearchInput from "./components/SearchInput";
+import Info from "./components/Info";
+import Todos from "./components/Todos";
+import Empty from "./components/Empty";
 
 function App() {
   const [value, setValue] = useState("");
   const [todos, setTodos] = useState([
-    {title: "Susu", count: 1},
-    {title: "Tahu", count: 1},
-    {title: "Apel", count: 1},
+    { title: "Susu", count: 1 },
+    { title: "Tahu", count: 1 },
+    { title: "Apel", count: 1 },
   ]);
 
   const handleSubmit = (e) => {
@@ -59,53 +61,26 @@ function App() {
       <Navbar />
 
       <Container>
-        <form className="form" onSubmit={handleSubmit}>
-          <input
-            className="input"
-            type="text"
-            placeholder="List"
-            onChange={(e) => {
-              setValue(e.target.value);
-            }}
-            value={value}
-          />
-          <button className="add-button">Add</button>
-        </form>
+        <SearchInput
+          onSubmit={handleSubmit} 
+          onChange={(e) => setValue(e.target.value)} 
+          value={value} 
+        />
 
-        <div className="info">
-            <div className="info-total">
-              <p>{`Total List: ${todos.length}`}</p>
-            </div>
-
-            <div className="info-total">
-              <p>{`Total Count: ${getTotalCount()}`}</p>
-            </div>
-
-            <button onClick={() => setTodos([])} className="delete-all-button">Delete All List</button>
-        </div>
+        <Info 
+          todosLength={todos.length} 
+          totalCount={getTotalCount()} 
+          onDelete={() => setTodos([])} 
+        />
 
         {todos.length > 0 ? (
-          <div className="todos">
-            {todos.map((todo, index, arr) => {
-              return (
-                <div key={index} className={`todo ${!(arr.length === index + 1) && 'todo-divider'}`}>
-                  {todo.title}
-                  <div className="todo-icon-wrapper">
-                    <div className="todo-count">{todo.count}</div>
-
-                    <button onClick={() => handleSubstractionCount(index)} className="todo-action-button">
-                      <img src={minusIcon} alt="minus icon" />
-                    </button>
-                    <button onClick={() => handleAdditionCount(index)} className="todo-action-button">
-                      <img src={plusIcon} alt="plus icon" />
-                    </button>
-                  </div>
-                </div>
-              )
-            })}
-          </div>
+          <Todos 
+            todos={todos} 
+            onSubstaction={(index) => handleSubstractionCount(index)} 
+            onAddition={(index) => handleAdditionCount(index)} 
+          />
         ) : (
-          <div>List Kosong</div>
+          <Empty />
         )}
       </Container>
     </>
---
+++ src/components/Empty/index.js
@@ -0,0 +1,9 @@
+import styles from "./Empty.module.css"
+
+const Empty = () => {
+    return (
+        <div className={styles.empty}>List Empty</div>
+    );
+};
+
+export default Empty;
